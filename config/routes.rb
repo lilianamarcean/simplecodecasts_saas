bae4e3bda35573f5d070191c8a2a92c9f417829b
Liliana Lil <jlolil@yahoo.com>
2016-07-07 18:46:41 +0000

    Added bootstrap classes and copy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get '/about' => 'pages#about'
-   get '/product' => 'pages#product'
   root 'pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
